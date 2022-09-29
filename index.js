4e7f4a885c0cc728db1e89994364959eed883b47
Glitch (glitch-hello-node) <none>
2022-09-29 17:36:36 +0000

    👩‍❤️‍💋‍👩🎉 Checkpoint ./index.js:7473097/1496 ./util/Commands.js:7473097/1571

--- index.js
+++ index.js
@@ -18,5 +18,54 @@ const client = new Discord.Client({
   } 
 });
 
-/* Modüller yüklendi */
+/* Modüller yüklendi. */
 
+client.once("ready", () => {
+  if(!global.client) {
+    global.client = client;
+  };
+});
+
+/* Client siteye tanımlandı. */
+
+function LoadUtils(utils) {
+  utils.forEach(async(e) => {
+     require('./util/' + e + '.js')(Discord, client, config);
+  });
+};
+
+/* 
+ @params {Array} names
+*/
+
+LoadUtils([ "Commands", "Events" ]);
+
+/* Sistem dosyaları yüklendi. */
+
+client.reload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./commands/${command}`)];
+            let cmd = require(`./commands/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            client.commands.set(command, cmd);
+            cmd.config.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.config.name);
+            });
+            resolve('[COMMANDS] '+command+" komutu yüklendi.");
+        } catch (e) {
+            reject(e.toString().includes('Cannot find module') ? 'Geçersiz komut adı yazdınız!': e);
+        }
+    });
+};
+
+/* Komutları yeniler. */
+
+client.login(config.token).then(() => {
+  console.log('(!) Token doğrulandı, giriş yapılıyor...');
+}).catch(() => {
+  console.error('(!) Token hatalı, kontrol ediniz...');
+});
