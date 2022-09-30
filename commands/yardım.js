f00d827a423d5696f39e593c1bbb1da95b2333fb
Glitch (glitch-hello-node) <none>
2022-09-30 07:20:03 +0000

    🚂🛣 Checkpoint ./commands/yardım.js:7473097/936

--- commands/yardım.js
+++ commands/yardım.js
@@ -6,12 +6,17 @@ const Discord = require('discord.js');
 module.exports = {
   run: async (client, message, args) => {
    
-  let commands = client.commands.filter(x => !x.config.enabled).map(x => `• **__${config.prefix+x.config.name}__** 🠮 ${x.config.desc}`).join("\n")
+  let commands = client.commands.filter(x => !x.config.enabled).map(x => `> • **__${config.prefix+x.config.name}__** 🠮 ${x.config.desc}`).join("\n")
     
   const embed = new Discord.EmbedBuilder()
   .setAuthor({ name: "Yardım menüsü | RomanBot", iconURL: client.user.displayAvatarURL({ dynamic: true })})
-  .setDescription(commands)
-  .setThumbn
+  .setDescription("• Yardım almak için en doğru yerdesin, bir sorunun olursa destek sunucusunda seni bekliyor olacağız.")
+  .addFields({ name: `🆕 **|** Yenilikler/Güncellemeler`, value: `> • Son güncelleme notları için [tıkla](https://discord.gg/E66J2HzraQ).` })
+  .addFields({ name: `📰 **|** Tüm komutlar`, value: `${commands} ` })
+  .setColor("#36393F")
+  .setThumbnail(`${client.user.displayAvatarURL({ dynamic: true })}`)
+  
+  message.reply({ embeds: [embed] })
 
 },
   config: {
