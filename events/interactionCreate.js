920d66f0a01aedef46bb95d24a5b91ab4c96e4df
ardacdx <116028863+ardacdx@users.noreply.github.com>
2022-12-31 14:48:27 +0300

    Delete events directory

--- events/interactionCreate.js
+++
@@ -1,6 +0,0 @@
-module.exports = (Discord, client, config) => {
-  client.on('interactionCreate', interaction => {
-      
-    
-  });
-};
--- events/ready.js
+++
@@ -1,6 +0,0 @@
-module.exports = (Discord, client, config) => {
-  client.on('ready', () => {
-      console.log('(!) ' + client.user.tag + ' olarak oturum açıldı!');
-      client.user.setPresence({ activities: [{ name: 'activity' }], status: 'idle' });
-  });
-};
